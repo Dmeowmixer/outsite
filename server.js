d3f662acfdfe0eaa03ab7723323f4d05e173b270
Dmeowmixer <ransonikuo@gmail.com>
2019-05-13 20:59:27 -1000

    has state set, and mock api rendered based on international/national. adds react-date-range and adds selected daterange to state. Currently working on deciphering how to query availability with daterange

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.get('/locations', (req, res) => {
         image: "https://uploads-ssl.webflow.com/5ab30f883c0d5f3ebc626470/5c00683b93af77a8fc105200_ny-header.jpg",
         numberOfBeds: 9
       }, {
-        id: "sc4",
+        id: "sc5",
         name: "Chile Santiago",
         city: "Santiago",
         country: "Chile",
--- src/Components/Locations.js
+++ src/Components/Locations.js
@@ -1,26 +1,76 @@
 import React, { Component } from 'react';
+import { DateRange } from 'react-date-range';
 
 class Locations extends Component {
-  state = {
-    locations: [],
-    data: []
+  constructor(props){
+    super(props);
+    this.handleSelect = this.handleSelect.bind(this);
   }
+  state = {
+    us:[],
+    international: [],
+    startDate: [],
+    endDate: []
+  }
+
   // Retrieve locations JSON through API
   fetchLocations = () => {
     fetch('/locations')
       .then(res => res.json())
-      .then(locations => this.setState({locations}));
+      .then(locations => {
+        const unitedStates = locations.filter(x => x.country === "US" );
+        const international = locations.filter(x => x.country !== "US");
+        this.setState({us: unitedStates});
+        this.setState({international: international});
+      })
+      // .then(locations => this.setState({ locations }))
+      // On fetch, map through or filter through the locations and insert them into international or us locations
   }
+
   componentDidMount(){
     this.fetchLocations();
   }
+
+  handleSelect(range){
+    this.setState({startDate: range.startDate});
+    this.setState({endDate: range.endDate});
+    // console.log(range.startDate);
+    // console.log(range.endDate._d);
+  }
+
   render() {
-    console.log(this.state.locations);
     return(
       <div className="locations">
-        <h1>Locations</h1>
+        <h3>Select your desired dates, and check available units</h3>
+        <DateRange
+          calendars="1"
+          onInit={this.handleSelect}
+          onChange={this.handleSelect}
 
-        <p>Hello World</p>
+        />
+        <h1>Locations</h1>
+        <h1>U.S.A</h1>
+        {this.state.us.map(location => 
+          <div key={location.id}>
+            <ul>
+              <li>{location.name}</li>
+              <li>{location.city}</li>
+              <li>{location.country}</li>
+              <li>{location.numberOfBeds}</li>
+            </ul>
+          </div>
+        )}
+        <h1>International</h1>
+        {this.state.international.map(location => 
+          <div key={location.id}>
+            <ul>
+              <li>{location.name}</li>
+              <li>{location.city}</li>
+              <li>{location.country}</li>
+              <li>{location.numberOfBeds}</li>
+            </ul>
+          </div>
+        )}
       </div>
     );
   }
